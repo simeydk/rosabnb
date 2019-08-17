250609c330a4ea1ff32f73ab08905c2ea43be524
Simey de Klerk <simeydeklerk@gmail.com>
2019-08-17 23:57:47 +0200

    Change Logo to function component

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,17 +1,12 @@
 import { Link } from "gatsby";
 import React from "react";
-export class Logo extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (<Link to="/" className="flex items-center no-underline text-white">
-            <img src='/images/rosa-logo.png' alt="Rosa B&B Logo" className="bg-white" style={{
+export function Logo() {
+    return (<Link to="/" className="flex items-center no-underline text-white">
+        <img src='/images/rosa-logo.png' alt="Rosa B&B Logo" className="bg-white" style={{
                 width: '200px',
                 position: "absolute",
                 top: "10px"
             }} />
 
         </Link>);
-    }
 }
