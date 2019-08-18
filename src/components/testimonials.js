1c47097ab9eedc49c03438d6a8094f52696dcb60
Simey de Klerk <simeydeklerk@gmail.com>
2019-08-18 22:28:10 +0200

    Add parallax background to testimonials

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,37 +1,59 @@
 import React from 'react';
+import { ParallaxBanner } from 'react-scroll-parallax';
 
 export default Testimonials
 
-
-
 const data = [
     {
-        text: "Felt like coming home! Very friendly staff, great and cozy rooms, excellent service and a marvelous breakfast.", 
+        text: "Felt like coming home! Very friendly staff, great and cozy rooms, excellent service and a marvelous breakfast.",
         author: "Ingrid",
         source: "TripAdvisor"
     },
     {
-        text: "Simply lovely.", 
+        text: "Simply lovely.",
         author: "Bea",
         source: "TripAdvisor"
     },
     {
-        text: "Central, clean, homely and very welcoming. Would definitely stay again.", 
+        text: "Central, clean, homely and very welcoming. Would definitely stay again.",
         author: "Dave",
         source: "Booking.com"
     },
 ]
 
-function Testimonial({text, author, source}) {
+function Testimonial({ text, author, source }) {
     return <div>
-        <h1 className="lg">“ {text}”</h1>
+        <h1 className="text-lg">“{text}”</h1>
         <p> - {author}, {source}</p>
         <p></p>
     </div>
 }
 
+function TestimonalBlock({ children }) {
+    const src = "https://res.cloudinary.com/simeydk/image/upload/v1566145346/Rosabnb/IMG_0424.jpg"
+    const style = {
+        height: "28rem",
+        maxHeight: "35vw",
+    }
+    const layers = [{ image: src, amount: 0.3 }]
+
+    return (
+        <div className="mx-auto max-w-5xl inset-shadow">
+            <ParallaxBanner layers={layers} style={style}>
+                <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+                    {children}
+                </div>
+            </ParallaxBanner>
+        </div>
+    )
+}
+
 function Testimonials() {
     return (
-        data.map(props => <Testimonial {...props}/>)
+        <TestimonalBlock>
+            <div className="bg-white max-w-sm px-10 py-6 m-20">
+                {data.map(props => <Testimonial {...props} />)}
+            </div>
+        </TestimonalBlock>
     )
 }
