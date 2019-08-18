eb5b504f059f0f77fe73b8e9aaabead8c6b539fc
Simey de Klerk <simeydeklerk@gmail.com>
2019-08-18 15:08:21 +0200

    Create FancyLogo element in separate component

---
+++ src/components/FancyLogo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function FancyLogo() {
+  return (<section className="flex flex-col justify-center justify-center px-2 py-4">
+    <h1 className="text-4xl uppercase text-center" style={{ color: "#94A485", fontWeight: "500" }}>
+      Rosa B&B
+    </h1>
+    <h2 className="text-lg italic serif with-lines text-center" style={{color:"#444"}}>Oudtshoorn</h2>
+
+  </section>)
+};
+
+export default FancyLogo;
+
