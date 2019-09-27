0e2b36909769aed754731995d940221118d93391
Simey de Klerk <simeydeklerk@gmail.com>
2019-09-27 15:53:13 +0200

    Tweak 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import abductionIllustration from "../images/abduction-illustration.svg";
 
 function NotFoundPage() {
   return (
