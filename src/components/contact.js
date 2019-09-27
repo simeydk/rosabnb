ae0d0692d2692aeab4ba72c61f8e28357089dd0b
Simey de Klerk <simeydeklerk@gmail.com>
2019-09-27 10:14:51 +0200

    Make contact form POST

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,7 +8,7 @@ function Contact() {
         <section className="m-4">
             <H2 id="contact">Contact us</H2>
             <p>{"info@rosabnb.co.za / 10 Vrede St, Oudtshoorn, 6620 / +27 (0) 65 962 2762"}</p>
-            <form name="contact" netlify >
+            <form name="contact" method="post" netlify >
                 <input type="text" placeholder="name" name="name" />
                 <input type="email" placeholder="email" name="email" />
                 <input type="text" placeholder="Subject" name="Subject" />
