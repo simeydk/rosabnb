a13c87071e392532fb28e1d05863a0bcab405e8f
Simey de Klerk <simeydeklerk@gmail.com>
2019-09-27 07:57:42 +0200

    Remove unused components in kids.js

--- src/components/kids.js
+++ src/components/kids.js
@@ -71,35 +71,3 @@ function KidsBlock({ children }) {
         </div>
     )
 }
-
-function TestimonalBlock({ children }) {
-    const src = "https://res.cloudinary.com/simeydk/image/upload/w_1200,h_1200,c_fit,f_auto,q_auto/Rosabnb/IMG_0424.jpg"
-    const style = {
-        // height: "28rem",
-        maxHeight: "75vw",
-    }
-    const layers = [{ image: src, amount: 0.3 }]
-
-    return (
-        <div className="mx-auto max-w-5xl inset-shadow">
-            <ParallaxBanner layers={layers} style={style}>
-                <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center inset-shadow">
-                    {children}
-                </div>
-            </ParallaxBanner>
-        </div>
-    )
-}
-
-function Testimonials() {
-    const entries = data.slice(0, 1).map(props => <Testimonial {...props} />)
-    return (
-        <TestimonalBlock>
-            <div className="bg-white max-w-sm m-4 p-4 md:px-10 md:py-6  md:m-20 outer-shadow">
-                {/* <Carousel> */}
-                {entries}
-                {/* </Carousel> */}
-            </div>
-        </TestimonalBlock>
-    )
-}
