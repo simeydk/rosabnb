6875fcd8bd925564291baba06b84d8222c60ae92
Simey de Klerk <simeydeklerk@gmail.com>
2019-09-27 07:56:53 +0200

    Create First version of gallery carousel

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,27 +1,68 @@
 import React from 'react';
+import {
+  ButtonBack,
+  ButtonFirst,
+  ButtonLast,
+  ButtonNext,
+  CarouselProvider,
+  DotGroup,
+  Dot,
+  Slide,
+  Slider,
+} from 'pure-react-carousel';
 import H2 from './utilities/H2'
+
 
 export default Gallery
 
 const data = [
-    "images/gallery/20170903_165147_HDR.jpg",
-    "images/gallery/20170904_115943_HDR.jpg",
-    "images/gallery/20170904_120120_HDR.jpg",
-    "images/gallery/IMG_0466.jpg",
-    "images/gallery/IMG_0494.jpg",
-    "images/gallery/IMG_0509.jpg",
-    "images/gallery/IMG_0612.JPG",
+    "Rosabnb/IMG_0424.jpg",
+    "Rosabnb/IMG_0417.jpg",
+    "Rosabnb/20170904_115943_HDR.jpg",
+    "Rosabnb/20170904_120120_HDR.jpg",
+    "Rosabnb/20170903_165147_HDR.jpg",
+    "Rosabnb/IMG_0494.jpg",
+    "Rosabnb/IMG_0612.jpg",
+    "Rosabnb/IMG_0509.jpg",
+    "Rosabnb/IMG_0466.jpg",
+    // "images/gallery/20170903_165147_HDR.jpg",
+    // "images/gallery/20170904_115943_HDR.jpg",
+    // "images/gallery/20170904_120120_HDR.jpg",
+    // "images/gallery/IMG_0466.jpg",
+    // "images/gallery/IMG_0494.jpg",
+    // "images/gallery/IMG_0509.jpg",
+    // "images/gallery/IMG_0612.JPG",
 ]
 
-const style = {
-    width:"25%",
-}
-
 function Gallery() {
-    return <section className="my-8" >
-        <H2  id="gallery">Gallery</H2>
-        <div className="flex flex-wrap">
-            {data.map(src => <img src={src} alt="" style={style}/>)}
-        </div>
-    </section>
+    return <section>
+    <CarouselProvider
+    visibleSlides={1}
+    totalSlides={data.length}
+    naturalSlideWidth={400}
+    naturalSlideHeight={200}
+    isPlaying
+  >
+    <H2>Gallery</H2>
+    <Slider>
+        {data.map((url,i) => (
+            <Slide tag="a" index={i}>
+                <img src={`https://res.cloudinary.com/simeydk/image/upload/w_1200,h_1200,c_fit,f_auto,q_auto/${url}`} alt="" />
+            </Slide>
+        ))}
+    </Slider>
+    <ButtonFirst>First</ButtonFirst>
+    <ButtonBack>Back</ButtonBack>
+    <ButtonNext>Next</ButtonNext>
+    <ButtonLast>Last</ButtonLast>
+    <div>
+        {data.map((url,i) =>(
+        <Dot slide={i}>
+            <img src={`https://res.cloudinary.com/simeydk/image/upload/w_100,h_100,c_fill,f_auto,q_auto/${url}`} alt="" />
+        </Dot>
+        ))}
+    </div>
+    <DotGroup dotNumbers />
+  </CarouselProvider>
+  </section>
 }
