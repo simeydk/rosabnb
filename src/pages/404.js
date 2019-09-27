39c3b8c6a9f1d4da4ea0af33e633a473b21e5697
Simey de Klerk <simeydeklerk@gmail.com>
2019-09-27 15:04:55 +0200

    Clean up unused pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,14 +8,9 @@ function NotFoundPage() {
   return (
     <Layout>
       <SEO title="404: Not found" />
-      <div>
-        <img
-          src={abductionIllustration}
-          className="block mx-auto w-1/2"
-          alt="Ghost getting abducted by aliens"
-        />
-        <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
-          Looks like this page is a ghost that got abducted by aliens...
+      <div>        
+        <h2 className="text-2xl font-bold inline-block my-8 p-3">
+          We were unable to find the page you were looking for
         </h2>
       </div>
     </Layout>
--- src/pages/lb.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Gallery from '../components/gallery'
-function IndexPage() {
-  return (
-          <Gallery />
-  );
-}
-
-export default IndexPage;
