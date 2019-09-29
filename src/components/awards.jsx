e1b37d6d3e57450fa63435cf052534136e0520b1
Simey de Klerk <simeydeklerk@gmail.com>
2019-09-29 07:55:17 +0200

    Move Awards logos to Cloudinary

--- src/components/awards.jsx
+++ src/components/awards.jsx
@@ -3,17 +3,18 @@ import H2 from './utilities/H2'
 
 export default Awards
 
-const logos = [
-    "images/logos/HolidayCheck.webp",
-    "images/logos/AA.webp",
-    "images/logos/Afristay.webp",
-]
 
-function Awards() {
+const logos = [
+    {src: "holiday_check.png", en: "Recommended on HolidayCheck", af: "Aanbeveel op HolidayCheck"},
+    {src: "afristay.jpg", en: "Afristay Traveller's Choice award", af: "Afristay Reisiger se Keuse toekenning"},
+    {src: "aa_quality_assured.jpg", en: "AA Quality Assured Highly recommended", af: "AA Quality Assured hoogs aanbeveel"},
+].map(({src, en, af}) => ({src: `https://res.cloudinary.com/simeydk/image/upload/Rosabnb/partner_logos/${src}`, en, af}))
+
+function Awards({lang = 'en'}) {
     return <section>
         <H2 id="awards">Awards</H2>
         <div className="flex flex-wrap align-items-center w-100 justify-center">
-        {logos.map(src => <img src={src} alt="award" className ="w-24 m-8"/>)}
+        {logos.map(logo => <img src={logo.src} alt={lang === 'en' ? logo.en : logo.af} className ="w-24 m-8"/>)}
         </div>
     </section>
 }
